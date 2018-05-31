33c8ca7ba7d996081581f80de3b775ff6332da85
Chris Van Patten <chris@vanpattenmedia.com>
2018-05-31 11:50:14 -0400

    📕 Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# PCS PHP
+
+A simple PHP client for Publisher's Creative System's [SOAP API](http://pcspublink.com/technology-and-tools/api-and-xml-web-services/).
+
+## Usage
+
+The client must be installed with Composer, as a manual VCS repository (it is not currently available in Packagist).
+
+To set up the client:
+
+```
+// Create a new instance of the API client with your pubcode and API password
+$pcs = new PCS('PUBCODE', 'API password');
+```
+
+To check if a user is active:
+
+```
+$status = $pcs->isUserActive('user@email.com', 'secret password');
+// returns true or false
+```
+
+More to come…
+
+## About Tomodomo
+
+Tomodomo is a creative agency for communities. We focus on unique design and technical solutions to grow community activity and increase customer retention for online networking forums and customer service communities.
+
+Learn more at [tomodomo.co](https://tomodomo.co) or email us: [hello@tomodomo.co](mailto:hello@tomodomo.co)
+
+## License & Conduct
+
+This project is licensed under the terms of the MIT License, included in `LICENSE.md`.
+
+All open source Tomodomo projects follow a strict code of conduct, included in `CODEOFCONDUCT.md`. We ask that all contributors adhere to the standards and guidelines in that document.
+
+Thank you!
---
+++ src/PCS.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Tomodomo\Client;
+
+class PCS
+{
+
+	/**
+	 * @var string
+	 */
+	var $url = 'https://subscribe.pcspublink.com/WSStatus_v1_1_6/WSStatus_v1_1_6.asmx';
+
+	/**
+	 * @param array $pubArgs
+	 * @return void
+	 */
+	public function __construct( $pubcode, $password )
+	{
+		// WSDL URL
+		$this->wsdl = $this->url . '?WSDL';
+
+		// SOAP Client
+		$this->client = new \SoapClient($this->wsdl);
+
+		// Set pubArgs to be used in SOAP headers
+		$this->pubArgs = [
+			'PubCode'  => $pubcode,
+			'Password' => $password,
+		];
+
+		return;
+	}
+
+	/**
+	 * Get a SoapHeader with our arguments
+	 *
+	 * @param array $args
+	 * @return SoapHeader
+	 */
+	public function getSoapHeader($name, $args = array())
+	{
+		$args = array_merge($this->pubArgs, $args);
+
+		return new \SoapHeader($this->url, $name, $args);
+	}
+
+	/**
+	 * Make a SOAP call and return a nice array
+	 *
+	 * @param string $name
+	 * @param SoapHeader $header
+	 * @return array
+	 */
+	public function makeSoapCall($name, $header)
+	{
+		// Make the SOAP API call to the client, with a header
+		$response = $this->client->__soapCall($name, [], [], [ $header ]);
+
+		// Get the Result
+		// @todo this is probably v fragile
+		$response = $response->{$name . 'Result'};
+
+		// Return a parsed response
+		return $this->getResponseAsArray($response);
+	}
+
+	/**
+	 * Get the response as an array instead of an XML string
+	 *
+	 * @param string $response
+	 * @return array
+	 */
+	public function getResponseAsArray($response = '')
+	{
+		// Load the XML
+		$xml = simplexml_load_string($response);
+
+		// Encode it as JSON
+		$json = json_encode($xml);
+
+		// Return it as an array
+		return json_decode($json, true);
+	}
+
+	/**
+	 * Check if a user is active from an email and password
+	 *
+	 * @param string $email
+	 * @param string $password
+	 * @return bool
+	 */
+	public function isUserActive($email, $password)
+	{
+		$args = [
+			'UserName'     => $email,
+			'UserPassword' => $password,
+		];
+
+		// Add args/auth header
+		$header = $this->getSoapHeader('ProfileAuthenticationHeader', $args);
+
+		// Get the response
+		$response = $this->makeSoapCall('GetIssuesFromProfile', $header);
+
+		return $response['Status'] ? true : false;
+	}
+
+	/**
+	 * Get a customer number from an email address, or 0 if
+	 * no customer number is available
+	 *
+	 * @param string $email
+	 * @return int
+	 */
+	public function getCustomerNumber($email)
+	{
+		$args = [
+			'EmailAddr' => $email,
+		];
+
+		// Add args/auth header
+		$header = $this->getSoapHeader('EmailAuthenticationHeader', $args);
+
+		// Get the response
+		$response = $this->makeSoapCall('GetCustomerNumberFromEmail', $header);
+
+		if (isset($response['CustomerNumber']) && is_numeric($response['CustomerNumber'])) {
+			return $response['CustomerNumber'];
+		}
+
+		return 0;
+	}
+
+	/**
+	 * Get a user's account info from their email address. User
+	 * must have an assigned customer number
+	 *
+	 * @param string $email
+	 * @return array
+	 */
+	public function getUserInfoFromEmail($email)
+	{
+		$customerNumber = $this->getCustomerNumber($email);
+
+		$args = [
+			'CustomerInetNumber' => $customerNumber,
+		];
+
+		// Add args/auth header
+		$header = $this->getSoapHeader('svc6CustInetAuthHeader', $args);
+
+		// Get the response
+		$response = $this->makeSoapCall('GetStatusExpDateIssRem', $header);
+
+		return $response;
+	}
+
+}
